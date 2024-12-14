00f1228ba5c17262f644bbf0bb4b18c35db5fa11
impap-byte <gokturkbnayytb@gmail.com>
2024-12-14 13:18:25 +0500

    Create Main.java

---
+++ e-school/Main.java
@@ -0,0 +1,20 @@
+import java.awt.GraphicsEnvironment;
+import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
+
+import javax.swing.ImageIcon;
+import javax.tools.JavaCompiler;
+
+public class Main{
+	
+	public static void main(String[] args) throws IOException {
+	//	String fonts[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+		//for(int i = 0;i<fonts.length;i++){
+		//	System.out.println(fonts[i]);
+	//	
+	Login log = new Login();
+	System.out.println();
+	
+	}
+
+}
